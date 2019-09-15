5ceaf6f62908716fba3d74cad04acc3d015f0e7f
mossnana <samurai.hanso@gmail.com>
2019-09-15 22:47:15 +0700

    HTML Templates Render

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # golang
 Practicing Golang 
+
+Run HTML Templates
+1. go mod init
+2. go build
+3. go mod vendor
+4. PORT=5000 ./golang
+5. Go to **localhost:5000**
---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main()  {
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("$PORT is not set.")
+	}
+	router := gin.New()
+	router.Use(gin.Logger())
+	router.LoadHTMLGlob("templates/*tmpl")
+	router.Static("/static", "static")
+	router.GET("/", func (c *gin.Context) {
+		c.HTML(http.StatusOK, "home.tmpl", nil)
+	})
+	router.Run(":" + port)
+}
